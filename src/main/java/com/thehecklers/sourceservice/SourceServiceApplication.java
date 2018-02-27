71a6a45725b893708085bcd6a67065ee5c51cc3f
Mark Heckler <mark.heckler@gmail.com>
2018-02-27 13:11:45 -0600

    Initial commit

---
+++ src/main/java/com/thehecklers/sourceservice/SourceServiceApplication.java
@@ -0,0 +1,47 @@
+package com.thehecklers.sourceservice;
+
+import lombok.Data;
+import lombok.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.UUID;
+
+@EnableBinding(Source.class)
+@EnableScheduling
+@SpringBootApplication
+public class SourceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SourceServiceApplication.class, args);
+    }
+}
+
+@Component
+class PingMaker {
+    private final Source source;
+
+    PingMaker(Source source) {
+        this.source = source;
+    }
+
+    @Scheduled(fixedRate = 1000)
+    public void PingIt() {
+        source.output().send(
+                MessageBuilder.withPayload(new Ping(UUID.randomUUID().toString(), new Date().toString())).build());
+    }
+}
+
+@Data
+@Value
+class Ping {
+    private final String id;
+    private final String payload;
+}
